07af6cbeb316b6d23623c16a9a43881f39ca85d6
rgprovencher <57636241+rgprovencher@users.noreply.github.com>
2021-10-18 17:14:17 +0000

    bug fixes

--- lab4.py
+++ lab4.py
@@ -11,9 +11,11 @@ with open('led-settings.txt', 'w') as f:
   # writes user-selected settings into a csv text file
   f.write("{},{}".format( data.getvalue('LED'),  data.getvalue('Brightness')  )
 
-print('Content-type: text/html\n\n')
+
 
 print("""
+
+Content-type: text/html\n\n
 
 <html>
 <form action =" /cgi-bin/demo/lab4.py" method="POST">
